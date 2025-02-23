7b0bb65b3a53da98a6860c29dd31e602b3da661c
noiqum <onurcoskun99@gmail.com>
2025-02-23 14:47:54 +0300

    book and category services

--- Services/IBookService.cs
+++ Services/IBookService.cs
@@ -7,9 +7,56 @@ namespace PagePortServer.Services
         IEnumerable<Book> GetAllBooks();
         Book GetBook(int id);
         Book AddBook(Book book);
-        Book UpdateBook(int id,Book book);
+        Book UpdateBook(int id, Book book);
         bool DeleteBook(int id);
 
 
+    }
+
+    public class BookService : IBookService
+    {
+        private static List<Book> _books = new List<Book>();
+
+        public IEnumerable<Book> GetAllBooks()
+        {
+            return _books;
+        }
+
+        public Book GetBook(int id)
+        {
+            return _books[id];
+        }
+
+        public Book AddBook(Book book)
+        {
+            _books.Add(book);
+            return book;
+        }
+
+        public Book UpdateBook(int id, Book book)
+        {
+            var existingBook = _books[id];
+            if (existingBook != null)
+            {
+                return null;
+            }
+            existingBook = book;
+            return existingBook;
+        }
+
+        public bool DeleteBook(int id)
+        {
+            var existingBook = _books[id];
+            if (existingBook != null)
+            {
+                return false;
+            }
+            if (existingBook != null)
+            {
+                _books.Remove(existingBook);
+                return true;
+            }
+            return false;
+        }
     }
 }
--- Services/ICategoryService.cs
+++ Services/ICategoryService.cs
@@ -4,13 +4,75 @@ namespace PagePortServer.Services
 {
     public interface ICategoryService
     {
-        IEnumerable <Category> GetCategories ();
-        Category GetCategory (int id);
+        IEnumerable<Category> GetCategories();
+        Category GetCategory(int id);
 
-        Category UpdateCategory (int id,Category category);
+        Category AddCategory(Category category);
 
-        bool DeleteCategory (int id);
+        Category UpdateCategory(int id, Category category);
 
-        IEnumerable<Book> GetCategoryBooks (int categoryId);
+        bool DeleteCategory(int id);
+
+        IEnumerable<Book> GetCategoryBooks(int categoryId);
     }
+
+    public class CategoryService : ICategoryService
+    {
+        private static List<Category> _categories = new List<Category>();
+        private static int _nextID = 1;
+
+        public IEnumerable<Category> GetCategories() => _categories;
+
+        public Category GetCategory(int id)
+        {
+            var category = _categories[id];
+            return category;
+        }
+
+
+        public Category AddCategory(Category category)
+        {
+            category.Id = _nextID++;
+            _categories.Add(category);
+            return category;
+        }
+
+        public Category UpdateCategory(int id, Category category)
+        {
+            var existingCategory = GetCategory(id);
+            if (existingCategory == null)
+                return null;
+
+            // Remove existing
+            _categories.Remove(existingCategory);
+
+            // Create new category with existing ID
+            var updatedCategory = new Category
+            {
+                Id = id,
+                Name = category.Name // Assuming your property is named 'Ad'
+            };
+
+            // Add updated category
+            _categories.Add(updatedCategory);
+
+            return updatedCategory;
+        }
+
+        public bool DeleteCategory(int id)
+        {
+            var existingCategory = GetCategory(id);
+            if (existingCategory == null) return false;
+            _categories.Remove(existingCategory);
+            return true;
+        }
+
+        public IEnumerable<Book> GetCategoryBooks(int categoryId)
+        {
+            var bookService = new BookService();
+            return bookService.GetAllBooks().Where(b => b.CategoryId == categoryId);
+        }
+
+    }
+
 }
